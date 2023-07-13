23fbbaf9ad116225d87afb6c76147f78224d4bfe
Fnckerpoi <38403438+Fnckerpoi@users.noreply.github.com>
2023-07-13 17:11:48 +0800

    删掉模型选项

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -492,11 +492,11 @@ export function ChatActions(props: {
         }}
       />
 
-      <ChatAction
-        onClick={() => setShowModelSelector(true)}
-        text={currentModel}
-        icon={<RobotIcon />}
-      />
+      {/*<ChatAction*/}
+      {/*  onClick={() => setShowModelSelector(true)}*/}
+      {/*  text={currentModel}*/}
+      {/*  icon={<RobotIcon />}*/}
+      {/*/>*/}
 
       {showModelSelector && (
         <Selector
